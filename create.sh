da973b208cab48f98d4d42551be83b3c88aa4795
Zaturrby <rjkorteschiel@gmail.com>
2016-09-12 19:59:33 +0200

    wip - end of day commit

--- create.sh
+++ create.sh
@@ -1,3 +1,3 @@
-aws cloudformation update-stack \
+aws cloudformation create-stack \
 --stack-name offcourse-web-production \
 --template-url https://s3-eu-west-1.amazonaws.com/offcourse-web-cf/root-template.json
--- validate.sh
+++ validate.sh
@@ -1,2 +1,2 @@
 aws cloudformation validate-template \
---template-url https://s3-eu-west-1.amazonaws.com/offcourse-web-cf/template.json
+--template-url https://s3-eu-west-1.amazonaws.com/offcourse-web-cf/root-template.json
