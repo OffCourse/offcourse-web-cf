47545947c8423e823db5a793ae875549f9781c0b
Zaturrby <rjkorteschiel@gmail.com>
2016-09-12 20:09:44 +0200

    add boot and fix double devStack

--- create.sh
+++ create.sh
@@ -1,3 +1,4 @@
+boot deploy
 aws cloudformation create-stack \
 --stack-name offcourse-web-production \
 --template-url https://s3-eu-west-1.amazonaws.com/offcourse-web-cf/root-template.json
--- validate.sh
+++ validate.sh
@@ -1,2 +1,3 @@
+boot deploy
 aws cloudformation validate-template \
 --template-url https://s3-eu-west-1.amazonaws.com/offcourse-web-cf/root-template.json
